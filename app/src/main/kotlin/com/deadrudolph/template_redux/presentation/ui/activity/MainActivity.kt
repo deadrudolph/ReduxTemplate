6b9db7459c97a6e075a89919a842265854409b35
deadrudolph <deadrudolph@gmail.com>
2024-03-17 18:11:11 -0400

    Changed name

--- app/src/main/kotlin/com/deadrudolph/template_redux/presentation/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/deadrudolph/template_redux/presentation/ui/activity/MainActivity.kt
@@ -41,7 +41,7 @@ internal class MainActivity : ComponentActivity(), ActivityActions {
 
     override fun getTheme(): Resources.Theme {
         val theme = super.getTheme()
-        theme.applyStyle(R.style.Theme_ComposeMultiModuleTemplate, true)
+        theme.applyStyle(R.style.Theme_Template, true)
         return theme
     }
 
